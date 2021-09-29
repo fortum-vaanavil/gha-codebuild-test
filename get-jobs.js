96ffa1cdfa94a81bf6be0518d7e0815e5946baa4
Ville Väänänen <ville.vaananen@fortum.com>
2021-09-29 21:09:12 +0300

    Get the jobs

--- get-jobs.js
+++ get-jobs.js
@@ -1,3 +1,10 @@
 module.exports = ({ github, context }) => {
-  return context.payload;
+  const run_id = context.payload.workflow_run.run_id;
+  const owner = context.repo.owner;
+  const repo = context.repo.repo;
+  return github.rest.actions.listJobsForWorkflowRun({
+    owner,
+    repo,
+    run_id,
+  });
 };
