95d127ed7e4e71e2a8a4fcd869d90bf429c80a2a
Ville Väänänen <ville.vaananen@fortum.com>
2021-09-29 21:15:58 +0300

    Only return the jobs

--- get-jobs.js
+++ get-jobs.js
@@ -1,10 +1,11 @@
-module.exports = ({ github, context }) => {
+module.exports = async ({ github, context }) => {
   const run_id = context.payload.workflow_run.id;
   const owner = context.repo.owner;
   const repo = context.repo.repo;
-  return github.rest.actions.listJobsForWorkflowRun({
+  const r = await github.rest.actions.listJobsForWorkflowRun({
     owner,
     repo,
     run_id,
   });
+  return r.data.jobs;
 };
